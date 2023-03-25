2509751669f940fa7623a03e90e25d5da1bd1975
Foiering Sergey <serjofoi@gmail.com>
2023-03-25 16:58:51 +0300

    learning Solidity

---
+++ StorageFactory.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory {
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(
+        uint256 _simpleStorageIndex,
+        uint256 _simpleStorageNumber
+    ) public {
+        // contract address
+        // ABI
+        // If it was an array of addresses would need to wrap with - SimpleStorage simpleStorage = SimpleStorage(simpleStorageArray[_simpleStorageIndex]);
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
+    }
+}
